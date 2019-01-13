79fdeab4ebf58b487ce879f4d967f187e91a0af7
freeman <Marajy@192.168.1.100>
2019-01-13 18:10:19 +0100

    Add interface and main methode

---
+++ jScrapr/src/main/java/com/klykoo/jScrapr/ScraprInterface.java
@@ -0,0 +1,20 @@
+package com.klykoo.jScrapr;
+
+public interface ScraprInterface {
+	
+	public float getPrice();
+
+	public String getTitle();
+
+	public String getDescription();
+
+	public Byte[][] getImages();
+
+	public String[] getLinks();
+	
+	public String getVendor();
+	
+	public String getRefVendor();
+	
+	public String getEan();
+}
---
+++ jScrapr/src/main/java/eCommerce/Amazonfr.java
@@ -0,0 +1,47 @@
+package eCommerce;
+
+import com.klykoo.jScrapr.ScraprInterface;
+
+public class Amazonfr implements ScraprInterface{
+
+	public float getPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Byte[][] getImages() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String[] getLinks() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getVendor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getRefVendor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getEan() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
